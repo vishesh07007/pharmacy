e14a7f1538105b695fe994dc2743d44b3edb4086
VISHESH <vishesh07007@gmail.com>
2025-09-26 22:41:19 +0530

    changes

--- src/main/java/com/sunrise/pharmacy/service/PrescriptionService.java
+++ src/main/java/com/sunrise/pharmacy/service/PrescriptionService.java
@@ -1,10 +1,13 @@
 package com.sunrise.pharmacy.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PrescriptionService {
 
+    @Autowired
+    
 
 
 }
