1b59d1528724690069ec6462ba7d8ee195620ac5
VISHESH <vishesh07007@gmail.com>
2025-09-28 23:45:08 +0530

    changes

--- src/main/java/com/sunrise/pharmacy/controller/HomeController.java
+++ src/main/java/com/sunrise/pharmacy/controller/HomeController.java
@@ -3,10 +3,13 @@ package com.sunrise.pharmacy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import ch.qos.logback.core.model.Model;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    
+    public String home(Model)
+
 
 }
