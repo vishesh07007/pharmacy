a992ba6eb2727b0e2c215ca06664ec835b0565be
VISHESH <vishesh07007@gmail.com>
2025-10-03 01:37:08 +0530

    changes

--- src/main/java/com/sunrise/pharmacy/repository/CustomerRepository.java
+++ src/main/java/com/sunrise/pharmacy/repository/CustomerRepository.java
@@ -1,10 +1,13 @@
 package com.sunrise.pharmacy.repository;
 
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sunrise.pharmacy.model.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
     Optional<Customer> findByPhone(String phone);
 
